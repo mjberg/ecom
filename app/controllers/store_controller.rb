ed094e816a49935fc49c897d052a413b027d496b
Matt Berg <matt@puresomni.com>
2020-02-12 18:08:49 -0600

    make a page counter

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,20 @@
 class StoreController < ApplicationController
-  def index
+
+
+
+
+   def index
+    @count = counter
+    @counter_message = "you've been to this page #{@count} times"
     @products = Product.order(:title)
   end
-end
+
+private
+
+  def counter
+    if session[:counter].nil?
+       session[:counter] = 0
+     end
+       session[:counter] += 1
+     end
+   end
