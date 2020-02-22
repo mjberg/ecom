6242576ea8c66a92b1f7926556334ee1cef9e67b
Matt Berg <matt@puresomni.com>
2020-02-22 13:02:17 -0500

    get user login scaffold up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::Base
+  before_action :authorize
+
+  #the before_action causes the autorize method to be invoked before every action in our applicaiton! 
+
+  protected
+
+  #putting methods in the protected area prevents them from ever being exposed to users as an action!
+
+  def authorize
+    unless User.find_by(id: session[:user_id])
+      redirect_to login_url, notice: "please login"
+    end
+  end
 end
