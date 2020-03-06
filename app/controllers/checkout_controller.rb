c9cc327820c589d49026c4c4d0e91e7dfc01143c
Matt Berg <matt@puresomni.com>
2020-03-06 07:42:15 -0500

    checkout controller

--- app/controllers/checkout_controller.rb
+++ app/controllers/checkout_controller.rb
@@ -2,12 +2,24 @@ class CheckoutController < ApplicationController
 
   def create
 
-    @carts = Cart.find(params[:id])
+    @cart = Cart.find(params[:cart_id])
 
-    @session = Stripe::Checkout::Session.create(
+    Stripe.api_key = 'sk_test_6EL9yHe00gASQ5yp6ZSXmTzQ'
+
+    session = Stripe::Checkout::Session.create(
       payment_method_types: ['card'],
-      line_items: []
+      line_items: @cart.map do |i|
+        data = {
+        name: i.product.description,
+        amount: i.product.price_cents,
+        currency: 'usd',
+        quantity: i.qty,
+      }
+    end,
+      success_url: checkout_success_url,
+      cancel_url: checkout_cancel_url,
     )
+
 
     respond_to do |format|
       format.js # render create.js.erb
