d8176d1bc4ea299068d4404dbc7cdf8355e493bd
Matt Berg <matt@puresomni.com>
2020-03-25 19:22:56 -0400

    clean up money display issue

--- app/models/product.rb
+++ app/models/product.rb
@@ -8,7 +8,6 @@ class Product < ApplicationRecord
   validates :price, numericality: { greater_than_or_equal_to: 0.01}
 
   has_many :line_items
-
   monetize :price_cents
 
   before_destroy :ensure_not_referenced_by_any_line_item
--- db/migrate/20200320154006_change_prict_to_big_decimal.rb
+++ db/migrate/20200320154006_change_prict_to_big_decimal.rb
@@ -1,7 +1,6 @@
 class ChangePrictToBigDecimal < ActiveRecord::Migration[5.2]
   def up
     change_column :products, :price, :integer
-    add_monetize :products, :price
 
   end
 
